26739c0ba8ef899b39589763af766a508c384091
JHome1 <112623883+JHome1@users.noreply.github.com>
2024-05-01 11:32:48 +1000

    Update train.sh

--- Point-Transformer-pointcam-online/train.sh
+++ Point-Transformer-pointcam-online/train.sh
@@ -1,4 +1,4 @@
 # PointTransformer
 # Open 'tool/train.sh', set 'exp_dir',
-# Open 'config/s3dis/s3dis_pointtransformer.yaml', set 'resume', 'cutmix', 'data_split' and 'open_eval'.
+# Open 'config/s3dis/s3dis_pointtransformer_repro.yaml', set 'resume', 'cutmix', 'data_split' and 'open_eval'.
 sh tool/train.sh s3dis pointtransformer_repro
